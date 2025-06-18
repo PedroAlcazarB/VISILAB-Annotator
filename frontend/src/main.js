ee2021d578481af7c1b4f4fbcb673637134013dd
Pedro Alcázar Barba <pedro.alcazar1@alu.uclm.es>
2025-06-18 18:19:18 +0200

    Añadido de Vue Konva al archivo main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import VueKonva from 'vue-konva'
 
 import HomeView from './views/HomeView.vue'
 import ProjectView from './views/ProjectView.vue'
 
 const app = createApp(App)
+app.use(VueKonva)
 
 // Configuración de Pinia
 const pinia = createPinia()
